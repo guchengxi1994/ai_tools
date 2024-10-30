a4f687a9b86de20ec683311d574b4d978d49705d
壹零 <guchengxi1994@qq.com>
2024-10-30 18:28:52 +0800

    stream

--- rust/src/api/llm.rs
+++ rust/src/api/llm.rs
@@ -1,3 +1,7 @@
+use flutter_rust_bridge::frb;
+
+use crate::{frb_generated::StreamSink, llm::{ChatResponse, CHAT_RESPONSE_SINK}};
+
 pub fn qwen2_chat(user_prompt: String, system_prompt: Option<String>, model_path: String) {
     let rt = tokio::runtime::Runtime::new().unwrap();
     let r = rt.block_on(async {
@@ -10,4 +14,11 @@ pub fn qwen2_chat(user_prompt: String, system_prompt: Option<String>, model_path
             println!("[rust-llm] Error: {}", _e);
         }
     }
+}
+
+#[frb(sync)]
+pub fn chat_response_stream(s:StreamSink<ChatResponse>) -> anyhow::Result<()>{
+    let mut stream = CHAT_RESPONSE_SINK.write().unwrap();
+    *stream = Some(s);
+    anyhow::Ok(())
 }
