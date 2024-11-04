e6f833f2e67f60402a36d3a7d1ebc4951f410d51
壹零 <guchengxi1994@qq.com>
2024-11-04 18:25:41 +0800

    Update mod.rs

--- rust/src/web_server/controllers/mod.rs
+++ rust/src/web_server/controllers/mod.rs
@@ -1,3 +1,5 @@
+use std::time::Duration;
+
 use actix_web::web::Bytes;
 use actix_web::{web, Error, HttpResponse};
 // use async_channel::{bounded, Receiver};
@@ -6,6 +8,8 @@ use futures::FutureExt;
 use log::error;
 use log::info;
 use tokio::sync::mpsc::{self, Receiver};
+use tokio::sync::Notify;
+use tokio::time::sleep;
 
 use crate::llm::qwen2::QWEN_MODEL;
 use crate::llm::BASE_TEMPLATE;
@@ -30,7 +34,7 @@ fn sse_stream(rx: Receiver<String>) -> impl Stream<Item = Result<Bytes, Error>> 
 pub async fn sse(req: web::Json<CompletionRequest>) -> HttpResponse {
     let (tx, rx) = mpsc::channel::<String>(1);
 
-    let _ = tx.send("data: [DONE]\n\n".to_string()).await;
+    let _ = tx.send("data: start\n\n".to_string()).await;
 
     {
         let mut global_model = QWEN_MODEL.write().unwrap();
@@ -52,21 +56,49 @@ pub async fn sse(req: web::Json<CompletionRequest>) -> HttpResponse {
             })));
     }
 
-    std::thread::spawn(move || {
-        let p = BASE_TEMPLATE.replace("{user}", &req.prompt);
+    let notify = Notify::new();
 
-        let mut global_model = QWEN_MODEL.write().unwrap();
-        let r = global_model
-            .as_mut()
-            .unwrap()
-            .run(&p, 1024, Some(tx.clone()));
-        match r {
-            Ok(_) => {}
-            Err(e) => {
-                error!("[rust-llm] Error: {}", e);
+    tokio::spawn({
+        let tx_clone = tx.clone();
+        async move {
+            loop {
+                // let _ = tx_clone.send("data: [DONE]\n\n".to_string()).await;
+                // tokio::time::sleep(Duration::from_millis(100)).await;
+                tokio::select! {
+                    // 等待线程1的通知
+                    _ = notify.notified() => {
+                        info!("Thread 2 exiting");
+                        break; // 收到通知后退出循环
+                    },
+                    _ = sleep(Duration::from_secs(1)) => {
+                        let _ = tx_clone.send("data: [DONE]\n\n".to_string()).await;
+                    },
+                }
             }
         }
     });
+
+    // tokio::spawn({
+    //     let tx_clone = tx.clone();
+
+    //     async move {
+    //         {
+    //             let p = BASE_TEMPLATE.replace("{user}", &req.prompt);
+
+    //             let mut global_model = QWEN_MODEL.write().unwrap();
+    //             let r = global_model
+    //                 .as_mut()
+    //                 .unwrap()
+    //                 .run(&p, 1024, Some(tx_clone.clone()));
+    //             match r {
+    //                 Ok(_) => {}
+    //                 Err(e) => {
+    //                     error!("[rust-llm] Error: {}", e);
+    //                 }
+    //             }
+    //         }
+    //     }
+    // });
 
     return HttpResponse::Ok()
         .content_type("text/event-stream")
