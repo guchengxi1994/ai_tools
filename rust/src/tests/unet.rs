898f8d919b85e85b284191680742ac8934ef6eaf
壹零 <guchengxi1994@qq.com>
2024-11-07 14:01:30 +0800

    unet train and test  code

--- rust/src/tests/unet.rs
+++ rust/src/tests/unet.rs
@@ -1,8 +1,13 @@
-use candle_core::{DType, Tensor};
+use std::io::{self, Write};
+
+use candle_core::{DType, Device, Tensor};
+use candle_dataset_loader::dataset::Dataset;
 use candle_nn::{
     batch_norm, conv2d, conv_transpose2d, BatchNorm, BatchNormConfig, Conv2d, ConvTranspose2d,
     ConvTranspose2dConfig, Module, ModuleT, VarBuilder, VarMap,
 };
+use candle_nn::{loss, Optimizer};
+use image::{DynamicImage, ImageReader};
 
 pub struct UNet {
     left1: DoubleConv,
@@ -22,32 +27,32 @@ pub struct UNet {
 }
 
 impl UNet {
-    pub fn new(vb: VarBuilder) -> anyhow::Result<Self> {
-        let left1 = DoubleConv::new("left1", 3, 64, vb.pp("left1"))?;
-        let left2 = DoubleConv::new("left2", 64, 128, vb.pp("left2"))?;
-        let left3 = DoubleConv::new("left3", 128, 256, vb.pp("left3"))?;
-        let left4 = DoubleConv::new("left4", 256, 512, vb.pp("left4"))?;
+    pub fn new(vb: VarBuilder, is_train: bool) -> anyhow::Result<Self> {
+        let left1 = DoubleConv::new("left1", 3, 64, vb.pp("left1"), is_train)?;
+        let left2 = DoubleConv::new("left2", 64, 128, vb.pp("left2"), is_train)?;
+        let left3 = DoubleConv::new("left3", 128, 256, vb.pp("left3"), is_train)?;
+        let left4 = DoubleConv::new("left4", 256, 512, vb.pp("left4"), is_train)?;
 
-        let center = DoubleConv::new("center", 512, 1024, vb.pp("center"))?;
+        let center = DoubleConv::new("center", 512, 1024, vb.pp("center"), is_train)?;
 
         let mut conv2d_config = ConvTranspose2dConfig::default();
         conv2d_config.stride = 2;
         let up1 = conv_transpose2d(1024, 512, 2, conv2d_config, vb.pp("up1"))?;
 
-        let right1 = DoubleConv::new("right1", 1024, 512, vb.pp("right1"))?;
+        let right1 = DoubleConv::new("right1", 1024, 512, vb.pp("right1"), is_train)?;
 
         let up2 = conv_transpose2d(512, 256, 2, conv2d_config, vb.pp("up2"))?;
-        let right2 = DoubleConv::new("right2", 512, 256, vb.pp("right2"))?;
+        let right2 = DoubleConv::new("right2", 512, 256, vb.pp("right2"), is_train)?;
 
         let up3 = conv_transpose2d(256, 128, 2, conv2d_config, vb.pp("up3"))?;
-        let right3 = DoubleConv::new("right3", 256, 128, vb.pp("right3"))?;
+        let right3 = DoubleConv::new("right3", 256, 128, vb.pp("right3"), is_train)?;
 
         let up4 = conv_transpose2d(128, 64, 2, conv2d_config, vb.pp("up4"))?;
-        let right4 = DoubleConv::new("right4", 128, 64, vb.pp("right4"))?;
+        let right4 = DoubleConv::new("right4", 128, 64, vb.pp("right4"), is_train)?;
 
         let output = conv2d(
             64,
-            3,
+            1,
             1,
             candle_nn::Conv2dConfig::default(),
             vb.pp("output"),
@@ -72,11 +77,43 @@ impl UNet {
     }
 }
 
+fn calculate_unet_output(input_size: usize) -> usize {
+    // 定义下采样的层数和每层卷积的像素减少量
+    let downsample_layers = 4;
+    let conv_reduction = 2; // 每次卷积减少 2 像素
+
+    // 初始大小
+    let mut size = input_size;
+
+    // 下采样阶段
+    for _ in 0..downsample_layers {
+        // 每层包含两次卷积，每次卷积减少 2 个像素
+        size -= conv_reduction * 2;
+        // 经过池化减半
+        size /= 2;
+    }
+
+    // 底部层卷积，执行两次卷积
+    size -= conv_reduction * 2;
+
+    // 上采样阶段
+    for _ in 0..downsample_layers {
+        // 上采样（尺寸加倍）
+        size *= 2;
+        // 每层包含两次卷积，每次卷积减少 2 个像素
+        size -= conv_reduction * 2;
+    }
+
+    // 返回最终输出层的尺寸
+    size
+}
+
 pub struct DoubleConv {
     conv1: Conv2d,
     bn1: BatchNorm,
     conv2: Conv2d,
     bn2: BatchNorm,
+    pub is_train: bool,
 }
 
 impl DoubleConv {
@@ -85,6 +122,7 @@ impl DoubleConv {
         in_channel: usize,
         out_channel: usize,
         vb: VarBuilder,
+        is_train: bool,
     ) -> anyhow::Result<Self> {
         let mut conv_cfg = candle_nn::Conv2dConfig::default();
         conv_cfg.padding = 1;
@@ -120,6 +158,7 @@ impl DoubleConv {
             bn1,
             conv2,
             bn2,
+            is_train,
         })
     }
 }
@@ -127,11 +166,70 @@ impl DoubleConv {
 impl candle_nn::Module for DoubleConv {
     fn forward(&self, xs: &candle_core::Tensor) -> candle_core::Result<candle_core::Tensor> {
         let xs = self.conv1.forward(xs)?;
-        let xs = self.bn1.forward_t(&xs, true)?;
+        let xs = self.bn1.forward_t(&xs, self.is_train)?;
         let xs = xs.relu()?;
         let xs = self.conv2.forward(&xs)?;
-        let xs = self.bn2.forward_t(&xs, true)?;
+        let xs = self.bn2.forward_t(&xs, self.is_train)?;
         Ok(xs.relu()?)
+    }
+}
+
+impl UNet {
+    pub fn infer(img: String, save: bool, binary: bool) -> anyhow::Result<Tensor> {
+        let d = Device::cuda_if_available(0)?;
+        let img = ImageReader::open(img)?.decode()?;
+        let mut t = img_to_tensor(img, 3, 256, 256, &d)?;
+        t = t.unsqueeze(0)?;
+        let mut varmap = VarMap::new();
+        varmap.load("unet_model.bin")?;
+        let vs = VarBuilder::from_varmap(&varmap, DType::F32, &d);
+        let net = Self::new(vs.clone(), false)?;
+        let r = net.forward(&t)?;
+        println!("output shape: {:?}", r.shape());
+        if save {
+            Self::save_tensor_as_image(&r, "output.png", 68, 68, binary)?
+        }
+        anyhow::Ok(r)
+    }
+
+    fn save_tensor_as_image(
+        tensor: &Tensor,
+        path: &str,
+        width: usize,
+        height: usize,
+        binary: bool,
+    ) -> anyhow::Result<()> {
+        // 将 Tensor 转换为 68x68 的 f32 向量
+        let data = tensor.squeeze(0)?.squeeze(0)?;
+        let data = data.flatten(0, 1)?.to_vec1::<f32>()?;
+
+        // 创建一个 68x68 的灰度图像
+        let mut img = image::GrayImage::new(width as u32, height as u32);
+
+        // 填充图像数据，进行归一化并转换为 u8 类型
+        for (i, pixel) in data.iter().enumerate() {
+            let x = (i % width) as u32;
+            let y = (i / height) as u32;
+            let p;
+            if binary {
+                if pixel < &0.5 {
+                    p = 0u8;
+                } else {
+                    p = 255u8;
+                }
+            } else {
+                p = (pixel * 255.0) as u8;
+            }
+
+            // 假设像素值在 0.0 到 1.0 范围内，映射到 0 到 255
+            let pixel_value = p;
+            img.put_pixel(x, y, image::Luma([pixel_value]));
+        }
+
+        // 保存图像
+        img.save(path)?;
+
+        Ok(())
     }
 }
 
@@ -160,7 +258,8 @@ impl candle_nn::Module for UNet {
         let xs = self.up1.forward(&xs)?;
         println!("up1 shape: {:?}", xs.shape());
 
-        let cropped1 = center_crop(&left4_clone, (56, 56))?;
+        // let cropped1 = center_crop(&left4_clone, (56, 56))?;
+        let cropped1 = center_crop(&left4_clone, (xs.shape().dims()[2], xs.shape().dims()[3]))?;
         println!("cropped1 shape: {:?}", cropped1.shape());
         let xs = candle_core::Tensor::cat(&[cropped1, xs], 1)?;
         let xs = self.right1.forward(&xs)?;
@@ -168,19 +267,22 @@ impl candle_nn::Module for UNet {
 
         let xs = self.up2.forward(&xs)?;
         println!("up2 shape: {:?}", xs.shape());
-        let cropped2 = center_crop(&left3_clone, (104, 104))?;
+        // let cropped2 = center_crop(&left3_clone, (104, 104))?;
+        let cropped2 = center_crop(&left3_clone, (xs.shape().dims()[2], xs.shape().dims()[3]))?;
         let xs = candle_core::Tensor::cat(&[cropped2, xs], 1)?;
         let xs = self.right2.forward(&xs)?;
         println!("right2 shape: {:?}", xs.shape());
 
         let xs = self.up3.forward(&xs)?;
-        let cropped3 = center_crop(&left2_clone, (200, 200))?;
+        // let cropped3 = center_crop(&left2_clone, (200, 200))?;
+        let cropped3 = center_crop(&left2_clone, (xs.shape().dims()[2], xs.shape().dims()[3]))?;
         let xs = candle_core::Tensor::cat(&[cropped3, xs], 1)?;
         let xs = self.right3.forward(&xs)?;
         println!("right3 shape: {:?}", xs.shape());
 
         let xs = self.up4.forward(&xs)?;
-        let cropped4 = center_crop(&left1_clone, (392, 392))?;
+        // let cropped4 = center_crop(&left1_clone, (392, 392))?;
+        let cropped4 = center_crop(&left1_clone, (xs.shape().dims()[2], xs.shape().dims()[3]))?;
         let xs = candle_core::Tensor::cat(&[cropped4, xs], 1)?;
         let xs = self.right4.forward(&xs)?;
         println!("right4 shape: {:?}", xs.shape());
@@ -217,10 +319,157 @@ fn test_unet() -> anyhow::Result<()> {
     let varmap = VarMap::new();
     let vs = VarBuilder::from_varmap(&varmap, DType::F32, &dev);
 
-    let unet = UNet::new(vs)?;
+    let unet = UNet::new(vs, false)?;
     let input = candle_core::Tensor::randn(1f32, 0.5f32, &[1, 3, 572, 572], &dev)?;
     let output = unet.forward(&input)?;
     println!("output shape: {:?}", output.shape());
 
     Ok(())
+}
+
+fn read_100_files(folder_path: String) -> anyhow::Result<Vec<String>> {
+    let mut files = Vec::new();
+    let mut count = 0;
+    for entry in std::fs::read_dir(folder_path)? {
+        let entry = entry?;
+        let path = entry.path();
+        if path.is_file() {
+            count += 1;
+            if count > 100 {
+                break;
+            }
+            files.push(path.display().to_string());
+        }
+    }
+    files.sort();
+    Ok(files)
+}
+
+fn files_to_tensor(
+    files: Vec<String>,
+    d: &Device,
+    width: u32,
+    height: u32,
+    depth: usize,
+) -> anyhow::Result<Tensor> {
+    let mut tensors: Vec<Tensor> = Vec::new();
+    for file in files {
+        let img = ImageReader::open(file)?.decode()?;
+        let t = img_to_tensor(img, depth, width, height, d)?;
+        tensors.push(t);
+    }
+
+    Ok(Tensor::stack(&tensors, 0)?)
+}
+
+fn img_to_tensor(
+    src: DynamicImage,
+    depth: usize,
+    width: u32,
+    height: u32,
+    d: &Device,
+) -> anyhow::Result<Tensor> {
+    if depth == 1 {
+        let i = DynamicImage::ImageLuma8(src.into_luma8()).resize_exact(
+            width,
+            height,
+            image::imageops::FilterType::Triangle,
+        );
+        let data = i.into_luma8().into_raw();
+        let data = Tensor::from_vec(data, (width as usize, height as usize, depth), &d)?
+            .permute((2, 0, 1))?;
+        let r = (data.to_dtype(DType::F32)? / 255.)?;
+        return Ok(r);
+    } else if depth == 3 {
+        let i = DynamicImage::ImageRgb8(src.to_rgb8()).resize_exact(
+            width,
+            height,
+            image::imageops::FilterType::Triangle,
+        );
+        // 转换为张量并归一化
+        let data = i.into_rgb8().into_raw();
+        let data = Tensor::from_vec(data, (width as usize, height as usize, depth), &d)?
+            .permute((2, 0, 1))?;
+        let r = (data.to_dtype(DType::F32)? / 255.)?;
+        return Ok(r);
+    } else {
+        panic!("Invalid depth value. Only 1 and 3 are supported.");
+    }
+}
+
+#[test]
+fn test_unet_size() {
+    println!("{:?}", calculate_unet_output(572));
+    println!("{:?}", calculate_unet_output(256));
+}
+
+#[test]
+fn train_unet() -> anyhow::Result<()> {
+    let d = Device::cuda_if_available(0)?;
+    let train_path = r"D:\github_repo\ai_tools\rust\assets\isic2018\train\images\";
+    let mask_path = r"D:\github_repo\ai_tools\rust\assets\isic2018\train\masks\";
+
+    let train_files = read_100_files(train_path.to_string())?;
+    let mask_files = read_100_files(mask_path.to_string())?;
+
+    let train_tensor = files_to_tensor(train_files, &d, 256, 256, 3)?;
+    let mask_tensor = files_to_tensor(mask_files, &d, 68, 68, 1)?;
+
+    println!("train_tensor shape: {:?}", train_tensor.shape());
+    println!("mask_tensor shape: {:?}", mask_tensor.shape());
+
+    let varmap = VarMap::new();
+    let vs = VarBuilder::from_varmap(&varmap, DType::F32, &d);
+    let model = UNet::new(vs.clone(), true)?;
+    let param = candle_nn::ParamsAdamW::default();
+    let mut adam = candle_nn::AdamW::new(varmap.all_vars(), param)?;
+
+    let d = Dataset {
+        train_data: train_tensor,
+        train_labels: mask_tensor,
+        test_data: None,
+        test_labels: None,
+        batch_size: 3,
+    };
+    let mut i = 0;
+    let _ = d.into_iter().take(300).for_each(|(x, y)| {
+        i += 1;
+        let results = model.forward(&x).unwrap();
+        let loss = loss::mse(&results, &y).unwrap();
+        println!("epoch: {}, loss: {}", i, loss);
+        io::stdout().flush().unwrap();
+        adam.backward_step(&loss).unwrap();
+    });
+
+    varmap.save("unet_model.bin")?;
+    anyhow::Ok(())
+}
+
+#[test]
+fn test_flatten() -> anyhow::Result<()> {
+    // 创建一个二维张量，例如大小为 3x4
+    let tensor = Tensor::new(
+        &[
+            [1.0, 2.0, 3.0, 4.0],
+            [5.0, 6.0, 7.0, 8.0],
+            [9.0, 10.0, 11.0, 12.0],
+        ],
+        &Device::Cpu,
+    )?;
+
+    // 将二维张量 flatten 到一维
+    let flattened_tensor = tensor.flatten(0, 1)?;
+    println!("{:?}", flattened_tensor.to_vec1::<f64>()?);
+    Ok(())
+}
+
+#[test]
+fn test_infer_unet() -> anyhow::Result<()> {
+    UNet::infer(
+        r"D:\github_repo\ai_tools\rust\assets\isic2018\train\images\0.png".to_owned(),
+        true,
+        false
+    )?;
+
+    anyhow::Ok(())
 }
